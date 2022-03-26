fb4095a0ae137080ec46811a54c9fa6c999b3e87
brenthaertlein <brenthaertlein@gmail.com>
2022-03-26 16:01:41 -0400

    rebrand as elden ring messenger ignore idea./ add elden ring svg add message templates build templates component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders elden ring message generator heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/elden ring message generator/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button, ButtonGroup, Card, Container} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = [
+
+    "**** ahead",
+    "Likely ****",
+    "If only I had a ****...",
+    "****, O ****",
+    "Ahh, ****...",
+    "No **** ahead",
+    "First off, ****",
+    "Didn't expect ****...",
+    "Behold, ****!",
+    "****",
+    "**** required ahead",
+    "Seek ****",
+    "Visions of ****...",
+    "Offer ****",
+    "****!",
+    "Be wary of ****",
+    "Still no ****...",
+    "Could this be a ****?",
+    "Praise the ****	****?",
+    "Try ****",
+    "Why is it always ****?",
+    "Time for ****",
+    "Let there be ****",
+    "****...",
+]
+
+const App = () => {
+    const [message, setMessage] = useState<string>('');
+    return (
+        <Container>
+            <h1>Elden Ring Message Generator</h1>
+            <p>Here we will generate some Elden Ring messages</p>
+            <div className="mb-3" style={{textAlign: 'center'}}>
+                {message}
+            </div>
+            <div>
+                <Card>
+                    <Card.Header>Templates</Card.Header>
+                    <Card.Body>
+                        {
+                            options.map(option => (
+                                <Button
+                                    className="m-1"
+                                    onClick={() => setMessage(option)}
+                                >
+                                    {option}
+                                </Button>
+                            ))
+                        }
+                    </Card.Body>
+                </Card>
+            </div>
+        </Container>
+    );
 }
 
 export default App;
