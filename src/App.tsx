f2a96b9f690d7dcdcce54e412cfaa1d37ca4cc43
brenthaertlein <brenthaertlein@gmail.com>
2022-03-27 12:15:23 -0400

    move static data to `data.json` move `Word` type to `Word.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,196 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Button, Card, Container} from 'react-bootstrap';
+import data from './data.json';
+import {Word} from './Word';
 
-const options = [
-    "**** ahead",
-    "Likely ****",
-    "If only I had a ****...",
-    "****, O ****",
-    "Ahh, ****...",
-    "No **** ahead",
-    "First off, ****",
-    "Didn't expect ****...",
-    "Behold, ****!",
-    "****",
-    "**** required ahead",
-    "Seek ****",
-    "Visions of ****...",
-    "Offer ****",
-    "****!",
-    "Be wary of ****",
-    "Still no ****...",
-    "Could this be a ****?",
-    "Praise the ****",
-    "****?",
-    "Try ****",
-    "Why is it always ****?",
-    "Time for ****",
-    "Let there be ****",
-    "****...",
-]
-
-const dictionary = {
-    'Enemies': [
-        'enemy',
-        'weak foe',
-        'strong foe',
-        'monster',
-        'dragon',
-        'boss',
-        'sentry',
-        'group',
-        'pack',
-        'decoy',
-        'undead',
-        'soldier',
-        'knight',
-        'cavalier',
-        'archer',
-        'sniper',
-        'mage',
-        'ordnance',
-        'monarch',
-        'lord',
-        'demi-human',
-        'outsider',
-        'giant',
-        'horse',
-        'dog',
-        'wolf',
-        'rat',
-        'beast',
-        'bird',
-        'raptor',
-        'snake',
-        'crab',
-        'prawn',
-        'octopus',
-        'bug',
-        'scarab',
-        'slug',
-        'wraith',
-        'skeleton',
-        'monstrosity',
-        'ill-omened creature',
-    ],
-    'People': [
-        'Tarnished',
-        'warrior',
-        'swordfighter',
-        'knight',
-        'samurai',
-        'sorcerer',
-        'cleric',
-        'sage',
-        'merchant',
-        'teacher',
-        'master',
-        'friend',
-        'lover',
-        'old dear',
-        'old codger',
-        'angel',
-        'fat coinpurse',
-        'pauper',
-        'good sort',
-        'wicked sort',
-        'plump sort',
-        'skinny sort',
-        'lovable sort',
-        'pathetic sort',
-        'strange sort',
-        'numble sort',
-        'laggardly sort',
-        'invisible sort',
-        'unfathomable sort',
-        'giant sort',
-        'sinner',
-        'thief',
-        'liar',
-        'dastard',
-        'traitor',
-        'pair',
-        'trio',
-        'noble',
-        'aristocrat',
-        'hero',
-        'champion',
-        'monarch',
-        'lord',
-        'god',
-    ],
-    'Things': [
-        'item',
-        'necessary item',
-        'precious item',
-        'something',
-        'something incredible',
-        'treasure chest',
-        'corpse',
-        'coffin',
-        'trap',
-        'armament',
-        'shield',
-        'bow',
-        'projectile weapon',
-        'armor',
-        'talisman',
-        'skill',
-        'sorcery',
-        'incantation',
-        'mao',
-        'material',
-        'flower',
-        'grass',
-        'tree',
-        'fruit',
-        'seed',
-        'mushroom',
-        'tear',
-        'crystal',
-        'butterfly',
-        'bug',
-        'dung',
-        'grace',
-        'door',
-        'key',
-        'ladder',
-        'lever',
-        'lift',
-        'spiritspring',
-        'sending gate',
-        'stone astrolabe',
-        'Birdseye Telescope',
-        'message',
-        'bloodstain',
-        'Erdtree',
-        'Elden Ring',
-    ],
-    'Battle Tactics': [],
-    'Actions': [],
-    'Situations': [],
-    'Places': [],
-    'Directions': [],
-    'Body Parts': [],
-    'Affinities': [],
-    'Concepts': [],
-    'Phrases': []
-} as Record<Word, string[]>
-
-type Word =
-    'Enemies' |
-    'People' |
-    'Things' |
-    'Battle Tactics' |
-    'Actions' |
-    'Situations' |
-    'Places' |
-    'Directions' |
-    'Body Parts' |
-    'Affinities' |
-    'Concepts' |
-    'Phrases'
+const {dictionary, templates} = data
 
 const App = () => {
     const [message, setMessage] = useState<string>('');
@@ -210,13 +24,13 @@ const App = () => {
                     <Card.Header>Templates</Card.Header>
                     <Card.Body>
                         {
-                            options.map(option => (
+                            (templates as string[]).map(template => (
                                 <Button
-                                    key={option}
+                                    key={template}
                                     className="m-1"
-                                    onClick={() => setMessage(option)}
+                                    onClick={() => setMessage(template)}
                                 >
-                                    {option}
+                                    {template}
                                 </Button>
                             ))
                         }
@@ -230,7 +44,7 @@ const App = () => {
                             <Card.Header>Words</Card.Header>
                             <Card.Body>
                                 {
-                                    Object.keys(dictionary).map(word => (
+                                    Object.keys(dictionary as Record<Word, string[]>).map(word => (
                                         <Button
                                             key={word}
                                             className="m-1"
